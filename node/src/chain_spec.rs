91847e469a457ea7b2dc9c0ab8535ae26807c763
gotoswap <2nb2nb@gmail.com>
2020-08-25 17:06:29 +0800

    update chain specification

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,16 +1,21 @@
-use sp_core::{Pair, Public, sr25519};
+use sp_core::{Pair, Public, crypto::UncheckedInto, sr25519};
 use node_template_runtime::{
 	AccountId, BabeConfig, BalancesConfig, GenesisConfig, GrandpaConfig,
-	SudoConfig, SystemConfig, WASM_BINARY, Signature
+	SudoConfig, SystemConfig, SessionConfig, StakingConfig, DemocracyConfig,
+	ElectionsConfig, CouncilConfig, TechnicalCommitteeConfig, opaque::SessionKeys,
+	StakerStatus, Balance, currency::DOLLARS, WASM_BINARY, Signature, ImOnlineConfig,
 };
-//use sp_consensus_aura::sr25519::AuthorityId as AuraId;
-use sp_consensus_babe::AuthorityId as BabeId;
+use sp_consensus_babe::{AuthorityId as BabeId};
 use sp_finality_grandpa::AuthorityId as GrandpaId;
-use sp_runtime::traits::{Verify, IdentifyAccount};
+use pallet_im_online::sr25519::{AuthorityId as ImOnlineId};
+use sp_runtime::{Perbill, traits::{Verify, IdentifyAccount}};
 use sc_service::ChainType;
+use hex_literal::hex;
+use telemetry::TelemetryEndpoints;
+use pallet_staking::Forcing;
 
 // Note this is the URL for the telemetry server
-//const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
+const RIC_STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
 
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
 pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig>;
@@ -31,13 +36,15 @@ pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId where
 	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
 }
 
-pub fn authority_keys_from_seed(s: &str) -> (AccountId, AccountId, BabeId, GrandpaId) {
-  (
-    get_account_id_from_seed::<sr25519::Public>(&format!("{}//stash", s)),
-    get_account_id_from_seed::<sr25519::Public>(s),
-    get_from_seed::<BabeId>(s),
-    get_from_seed::<GrandpaId>(s),
-  )
+/// helper function to generate an authority key for Babe
+pub fn authority_keys_from_seed(s: &str) -> (AccountId, AccountId, BabeId, GrandpaId, ImOnlineId) {
+	(
+		get_account_id_from_seed::<sr25519::Public>(&format!("{}//stash", s)),
+		get_account_id_from_seed::<sr25519::Public>(s),
+		get_from_seed::<BabeId>(s),
+		get_from_seed::<GrandpaId>(s),
+		get_from_seed::<ImOnlineId>(s),
+	)
 }
 
 pub fn development_config() -> ChainSpec {
@@ -75,10 +82,10 @@ pub fn local_testnet_config() -> ChainSpec {
 			vec![
 				authority_keys_from_seed("Alice"),
 				authority_keys_from_seed("Bob"),
-        authority_keys_from_seed("Charlie"),
-        authority_keys_from_seed("Dave"),
-        authority_keys_from_seed("Eve"),
-        authority_keys_from_seed("Ferdie"),
+        		authority_keys_from_seed("Charlie"),
+        		authority_keys_from_seed("Dave"),
+        		authority_keys_from_seed("Eve"),
+        		authority_keys_from_seed("Ferdie"),
 			],
 			get_account_id_from_seed::<sr25519::Public>("Alice"),
 			vec![
@@ -105,10 +112,22 @@ pub fn local_testnet_config() -> ChainSpec {
 	)
 }
 
-fn testnet_genesis(initial_authorities: Vec<(AccountId, AccountId, BabeId, GrandpaId)>,
+fn session_keys(
+	babe: BabeId,
+	grandpa: GrandpaId,
+	im_online: ImOnlineId
+) -> SessionKeys {
+	SessionKeys { grandpa, babe, im_online }
+}
+
+fn testnet_genesis(initial_authorities: Vec<(AccountId, AccountId, BabeId, GrandpaId, ImOnlineId)>,
 	root_key: AccountId,
 	endowed_accounts: Vec<AccountId>,
-	_enable_println: bool) -> GenesisConfig {
+	_enable_println: bool
+) -> GenesisConfig {
+	const STASH: Balance = 100 * DOLLARS;
+	let num_endowed_accounts = endowed_accounts.len();
+
 	GenesisConfig {
 		system: Some(SystemConfig {
 			code: WASM_BINARY.to_vec(),
@@ -118,13 +137,53 @@ fn testnet_genesis(initial_authorities: Vec<(AccountId, AccountId, BabeId, Grand
 			balances: endowed_accounts.iter().cloned().map(|k|(k, 1 << 60)).collect(),
 		}),
 		babe: Some(BabeConfig {
-      authorities: initial_authorities.iter().map(|x| (x.2.clone(), 1)).collect(),
-    }),
+			authorities: vec![],
+		}),
 		grandpa: Some(GrandpaConfig {
-			authorities: initial_authorities.iter().map(|x| (x.3.clone(), 1)).collect(),
+			authorities: vec![],
 		}),
 		sudo: Some(SudoConfig {
 			key: root_key,
 		}),
+		session: Some(SessionConfig {
+			keys: initial_authorities.iter().map(|x| {
+				(x.0.clone(), x.0.clone(), session_keys(
+					x.2.clone(),
+					x.3.clone(),
+					x.4.clone(),
+				))
+			}).collect::<Vec<_>>(),
+		}),
+		staking: Some(StakingConfig {
+			validator_count: initial_authorities.len() as u32 * 2,
+			minimum_validator_count: initial_authorities.len() as u32,
+			stakers: initial_authorities.iter().map(|x| {
+				(x.0.clone(), x.1.clone(), STASH, StakerStatus::Validator)
+			}).collect(),
+			invulnerables: initial_authorities.iter().map(|x| x.0.clone()).collect(),
+			slash_reward_fraction: Perbill::from_percent(10),
+			.. Default::default()
+		}),
+		im_online: Some(ImOnlineConfig {
+			keys: vec![],
+		}),
+		democracy: Some(DemocracyConfig::default()),
+		elections_phragmen: Some(ElectionsConfig {
+			members: endowed_accounts.iter()
+						.take((num_endowed_accounts + 1) / 2)
+						.cloned()
+						.map(|member| (member, STASH))
+						.collect(),
+		}),
+		collective_Instance1: Some(CouncilConfig::default()),
+		collective_Instance2: Some(TechnicalCommitteeConfig {
+			members: endowed_accounts.iter()
+						.take((num_endowed_accounts + 1) / 2)
+						.cloned()
+						.collect(),
+			phantom: Default::default(),
+		}),
+		membership_Instance1: Some(Default::default()),
+		treasury: Some(Default::default()),
 	}
 }
